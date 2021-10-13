01428805888bc28f3ed2ec2642c6469f06e7a08b
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-13 16:35:39 -0300

    desafio 1 - adicionar e mostrar campo em todos os documentos

---
+++ challenges/desafio1.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+    {},
+    {
+      $set: {
+        criadoPor: "Ronald McDonald",
+      },
+    },
+  );
+  
+  db.produtos.find(
+    {},
+    {
+      _id: false,
+      nome: true,
+      criadoPor: true,
+    },
+  );
