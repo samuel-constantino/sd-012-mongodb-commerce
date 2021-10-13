14162b24a0c4dc29776ba77eddec2b4b099fd864
Samuel Constantino <samuelicapuidados@gmail.com>
2021-10-13 17:35:46 -0300

    desafio 4 - adiciona campo com tipo Date

---
+++ challenges/desafio4.js
@@ -0,0 +1,20 @@
+db.produtos.updateOne(
+  {
+    nome: "Big Mac",
+  },
+  {
+    $set: {
+      ultimaModificacao: Date(),
+    },
+  },
+);
+
+db.produtos.find(
+  {
+    ultimaModificacao: { $exists: true },
+  },
+  {
+    _id: false,
+    nome: true,
+  },
+);
